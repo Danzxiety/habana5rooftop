d01874c9f6e6308d25143781e65bd1fd2588ecaa
Danzxiety <uixsoftware@gmail.com>
2024-05-12 19:51:37 +0000

    035112

--- assets/php/load_products_business.php
+++ assets/php/load_products_business.php
@@ -36,12 +36,6 @@ if ($result->num_rows > 0) {
         echo '</div>';     
     }
 
-echo '<div class="col-lg-1 mb-3" style="align-content: center; justify-content: center !important;align-items: center;margin-left: -10px !important;">
-<a href="#" data-bs-toggle="modal" data-bs-target="#add-products" class="btn btn-light icmenu p-2 rounded-pill justify-content-center align-items-center" style="align-content: center; background-color: rgba(189, 197, 209, .2);">
-<svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 12H18M12 6V18" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-</a>
-</div>';
-
 
 
 } else {
